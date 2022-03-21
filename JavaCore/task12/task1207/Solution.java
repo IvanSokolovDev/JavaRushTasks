57a37131fbc61e631ebd09531a046a278a805d95
IvanSokolovDev <99478390+IvanSokolovDev@users.noreply.github.com>
2022-03-21 19:40:20 +0300

    Add files via upload

---
+++ JavaCore/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int paramInt = 5;
+        print(paramInt);
+
+        Integer paramInteger = 6;
+        print(paramInteger);
+    }
+
+    public static void print(int param){
+
+    }
+
+    public static void print(Integer param){
+
+    }
+}
---
+++ JavaCore/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int param){
+
+    }
+
+    public static void print(float param){
+
+    }
+
+    public static void print(boolean param){
+
+    }
+
+    public static void print(char param){
+
+    }
+
+    public static void print(String param){
+
+    }
+}
